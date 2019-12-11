046d4663f3e0901a610b6561784fe04e08780fdc
DmitryPolyakov1985 <dps.coding@gmail.com>
2019-12-10 22:31:16 -0600

    5 day forecast started

--- src/pages/FiveDayForecast.js
+++ src/pages/FiveDayForecast.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+const apiKey = '4fad5043f986c8cf84c29a4a3e7c3f49';
 
 function FiveDayForecast() {
+    const [forecast, setForecast] = useState('');
+
+    useEffect(function() {
+        async function fetchData() {
+            const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=London,uk&mode=json&units=metric&APPID=${apiKey}`)
+            const jsonData = await response.json()
+            console.log(jsonData)
+            setForecast({
+                city: jsonData.city.name
+            }); 
+        }
+        fetchData();
+           
+    }, [])
+
     return (
         <div>
-            This is a five day forecast
+            {forecast.city}
         </div>
     );
 }
