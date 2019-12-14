81d21c3d4a51fd884c874365b54f9bcdfa48a40f
DmitryPolyakov1985 <dps.coding@gmail.com>
2019-12-14 13:41:42 -0600

    Current weather styles added

--- src/Weather.js
+++ src/Weather.js
@@ -3,25 +3,27 @@ import './Weather.css';
 
 function Weather( props ) {
     return (
-        <div>
-            <div className='thumbnail'>
+        <div className='weatherContainer'>
+            <div className='weatherBox'>
                 {props.date && <p>{props.date}</p>}
                 {props.icon && <img className='icon' src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} />}
-                {props.minTemp && <p>Min temperature: {props.minTemp}<sup>o</sup>C</p>}
-                {props.maxTemp && <p>Max temperature: {props.maxTemp}<sup>o</sup>C</p>}
+                <div className="weatherBoxTemperature">
+                    {props.minTemp && <p className='minTemp'>{props.minTemp}<sup>o</sup>C</p>}
+                    {props.maxTemp && <p className='maxTemp'>{props.maxTemp}<sup>o</sup>C</p>}
+                </div>
             </div>
 
-            {props.city && <p>City: {props.city}</p>}
-            {props.country && <p>Country: {props.country}</p>}
-            {props.temperature && <p>Temperature: {props.temperature}<sup>o</sup>C</p>}
-            {props.feelsLikeTemp && <p>Feels like: {props.feelsLikeTemp}<sup>o</sup>C</p>}
-            
-            {props.description && <p>Description: {props.description}</p>}
-            {props.wind && <p>Wind: {props.wind} m/s</p>}
-            {props.clouds && <p>Clouds: {props.clouds}%</p>}
-            {props.humidity && <p>Humidity: {props.humidity}%</p>}
-            {props.icon && <p>Icon: {props.icon}</p>}
-            {props.error && <p>Error: {props.error}</p>}
+            <div className="weaterData">
+                {props.city && <p><strong>City: </strong> {props.city}</p>}
+                {props.country && <p>Country: {props.country}</p>}
+                {props.temperature && <p>Temperature: {props.temperature}<sup>o</sup>C</p>}
+                {props.feelsLikeTemp && <p>Feels like: {props.feelsLikeTemp}<sup>o</sup>C</p>}
+                {props.description && <p>Description: {props.description}</p>}
+                {props.wind && <p>Wind: {props.wind} m/s</p>}
+                {props.clouds && <p>Clouds: {props.clouds}%</p>}
+                {props.humidity && <p>Humidity: {props.humidity}%</p>}
+                {props.error && <p>Error: {props.error}</p>}
+            </div>
             
         </div>
     );
