06d6d11fd5f5067e14083bd24e2eb3e8d4e37b92
DmitryPolyakov1985 <dps.coding@gmail.com>
2019-12-15 13:29:36 -0600

    Current weather is ready

--- src/Weather.js
+++ src/Weather.js
@@ -6,23 +6,23 @@ function Weather( props ) {
         <div className='weatherContainer'>
             <div className='weatherBox'>
                 {props.date && <p>{props.date}</p>}
-                {props.icon && <img className='icon' src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} />}
+                {props.icon && <img className='icon' src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt='Weather Icon'/>}
                 <div className="weatherBoxTemperature">
                     {props.minTemp && <p className='minTemp'>{props.minTemp}<sup>o</sup>C</p>}
                     {props.maxTemp && <p className='maxTemp'>{props.maxTemp}<sup>o</sup>C</p>}
                 </div>
             </div>
 
-            <div className="weaterData">
+            <div className="weatherData">
                 {props.city && <p><strong>City: </strong> {props.city}</p>}
-                {props.country && <p>Country: {props.country}</p>}
-                {props.temperature && <p>Temperature: {props.temperature}<sup>o</sup>C</p>}
-                {props.feelsLikeTemp && <p>Feels like: {props.feelsLikeTemp}<sup>o</sup>C</p>}
-                {props.description && <p>Description: {props.description}</p>}
-                {props.wind && <p>Wind: {props.wind} m/s</p>}
-                {props.clouds && <p>Clouds: {props.clouds}%</p>}
-                {props.humidity && <p>Humidity: {props.humidity}%</p>}
-                {props.error && <p>Error: {props.error}</p>}
+                {props.country && <p><strong>Country: </strong>{props.country}</p>}
+                {props.temperature && <p><strong>Temperature: </strong> {props.temperature}<sup>o</sup>C</p>}
+                {props.feelsLikeTemp && <p><strong>Feels like: </strong> {props.feelsLikeTemp}<sup>o</sup>C</p>}
+                {props.description && <p><strong>Description: </strong> {props.description}</p>}
+                {props.wind && <p><strong>Wind: </strong> {props.wind} m/s</p>}
+                {props.clouds && <p><strong>Clouds: </strong> {props.clouds}%</p>}
+                {props.humidity && <p><strong>Humidity: </strong> {props.humidity}%</p>}
+                {props.error && <p><strong>Error: </strong> {props.error}</p>}
             </div>
             
         </div>
