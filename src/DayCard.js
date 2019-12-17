e8c2f85d14d9071c843fb9a7558ab3ded2ca2d0c
DmitryPolyakov1985 <dps.coding@gmail.com>
2019-12-17 07:25:34 -0600

    Working

--- src/DayCard.js
+++ src/DayCard.js
@@ -4,7 +4,7 @@ import './DayCard.css';
 const moment = require('moment');
 
 function DayCard(props) {
-    console.log(props)
+    // console.log(props)
     let newDate = new Date();
     const weekday = props.reading.dt * 1000
     newDate.setTime(weekday)
