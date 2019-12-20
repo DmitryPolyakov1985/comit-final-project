a6ff41fd8b13edc21488e52c7aaeda6babd74b9c
DmitryPolyakov1985 <dps.coding@gmail.com>
2019-12-19 18:08:37 -0600

    Working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div>
             <h1>My Weather App for ComIT</h1>
-            
+            <h1 style={{ color: 'rgb(242, 61, 48)', padding: '30px', fontSize: '70px' }}>Congratulations on completion of React course</h1>
         </div>
     );
 }
