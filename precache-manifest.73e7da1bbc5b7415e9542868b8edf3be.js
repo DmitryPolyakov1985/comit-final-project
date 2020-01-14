6c56d332c6a300f60873347c0f623a63e896e132
DmitryPolyakov1985 <dps.coding@gmail.com>
2020-01-13 18:20:50 -0600

    Updates

---
+++ precache-manifest.73e7da1bbc5b7415e9542868b8edf3be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9662c0a54a25af7cbf46d7fa4158c452",
+    "url": "/comit-final-project/index.html"
+  },
+  {
+    "revision": "fcbdbc97fb4c54c099c2",
+    "url": "/comit-final-project/static/css/main.354219db.chunk.css"
+  },
+  {
+    "revision": "55699becf053904eda41",
+    "url": "/comit-final-project/static/js/2.fc92aefe.chunk.js"
+  },
+  {
+    "revision": "fcbdbc97fb4c54c099c2",
+    "url": "/comit-final-project/static/js/main.fd703d73.chunk.js"
+  },
+  {
+    "revision": "5a04b1db535aac1da1c4",
+    "url": "/comit-final-project/static/js/runtime-main.7b1809e5.js"
+  }
+]);
